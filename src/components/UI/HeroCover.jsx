c6cf0c37b5e340ef19123f657033bdf46a8f8b49
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2022-07-21 20:25:54 +0200

    Fix error

--- src/components/UI/HeroCover.jsx
+++ src/components/UI/HeroCover.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import classes from "./HeroCover.module.css";
-import HeroImg from "../../assets/images/6502019 1.png";
 
 const HeroCover = (props) => {
   return (
     <div className={`${classes.textImgWraper} ${props.className}`}>
       <div className={classes.imgWraper}>
-        <img
-          src={`${props.img.path}/portrait_incredible.${props.img.extension}`}
-          alt="hero"
-          className={classes.img}
-        />
+        {props.img && (
+          <img
+            src={`${props.img.path}/portrait_incredible.${props.img.extension}`}
+            alt="hero"
+            className={classes.img}
+          />
+        )}
         <div className={classes.imgDark}></div>
       </div>
       <h3>MARVEL</h3>
